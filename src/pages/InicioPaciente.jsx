8923dc6f8dbfc96ed4bef0f430e97c3ee983d830
facc77 <ferrerfacundo96@gmail.com>
2021-05-04 20:56:09 -0300

    fixing css issues in navbar

--- src/pages/InicioPaciente.jsx
+++ src/pages/InicioPaciente.jsx
@@ -9,7 +9,7 @@ import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Buscador from "../components/Buscador.jsx";
 
-export default function PruebaPacientes() {
+export default function InicioPaciente() {
   return (
     <div className="inicioPaciente">
       <div className="d-flex" id="content-wrapper">
@@ -103,32 +103,56 @@ export default function PruebaPacientes() {
               </div>
             </div>
           </nav> */}
-          <Navbar bg="light" expand="lg">
+          <Navbar bg="light" expand="lg" className="border-bottom py-3">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Form inline>
+              {/* <Form inline>
                 <FormControl
                   type="text"
                   placeholder="Search"
                   className="mr-sm-2"
                 />
-                <button className="btn position-absolute searchButton">
-                  <FontAwesomeIcon icon="search" className=" searchIcon" />
-                </button>
-              </Form>
+                <div className="input-group-append">
+                  <button className="btn searchButton">
+                    <FontAwesomeIcon icon="search" className=" searchIcon" />
+                  </button>
+                </div>
+              </Form> */}
+              <div className="searchContainer">
+                {" "}
+                <div className="input-group">
+                  <input
+                    type="search"
+                    placeholder="Busqueda"
+                    aria-describedby="button-addon1"
+                    className="form-control bg-light busquedaPlaceholder"
+                  />
+                  <div className="input-group-append">
+                    <button
+                      id="button-addon1"
+                      type="submit"
+                      className="btn btn-link text-primary"
+                    >
+                      <FontAwesomeIcon icon="search" />
+                    </button>
+                  </div>
+                </div>
+              </div>
+
               <Nav className="ml-auto">
-                <NavDropdown title="Cuenta paciente" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
+                <NavDropdown
+                  title="Cuenta paciente"
+                  id="basic-nav-dropdown"
+                  className="pacienteDropdown"
+                >
+                  <NavDropdown.Item href="/inicioPaciente">
+                    Buscar Medicos
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
+                  <NavDropdown.Item href="/turnosReservados">
+                    Turnos Reservados
                   </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/">Cerrar Sesion </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
             </Navbar.Collapse>
